4671f784cee34cd5908c000bf2f7a47922ad6ce6
alestor123 <alestoraldous@gmail.com>
2021-01-26 16:07:30 +0530

    added cli code

--- App.js
+++ App.js
@@ -3,6 +3,6 @@ api = [];
 module.exports =  async name => {
 if(!name) throw Error('Name not found')
 var data = await axios.get(`https://registry.npmjs.org/${name}`)
-Object.entries(data.data.versions).map(names => api.push({version : names[1].version , licence: names[1].license || 'No Licence'}))
+Object.entries(data.data.versions).map(names => api.push({version : names[1].version , license: names[1].license || 'No Licence'}))
 return api
 }
--- cli.js
+++ cli.js
@@ -1,1 +1,7 @@
 #!/usr/bin/env node
+var licence = require('./App'),
+chalk = require('chalk');
+(async () => {
+var data = await licence(process.argv[2]);
+data.map((names , index ) => console.log(chalk.cyanBright.bold(` \n ${data.length - index} : Version :  ${names.version} : ${names.license} \n `)))
+})();
